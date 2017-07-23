bdcb5b79a8080c47cdf07d47d0a81468b8bda3e9
Yasin Erduran <yasin.herduran@gmail.com>
2017-07-24 01:06:19 +0300

    added doctest

---
+++ doctestExample.py
@@ -0,0 +1,24 @@
+def make_matrix(rows, columns):
+    """
+      >>> make_matrix(3, 5)
+      [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+      >>> make_matrix(2, 1)
+      [[0], [0]]
+      >>> m = make_matrix(4, 2)
+      >>> m[1][1]=7
+      >>> m
+      [[0, 0], [0, 7], [0, 0], [0, 0]]
+    """
+    matrix = []
+    for row in range(rows):
+        matrix += [[0] * columns]
+    return matrix
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+m = make_matrix(4 ,3)
+m[1][2]= 7
+print(m)
