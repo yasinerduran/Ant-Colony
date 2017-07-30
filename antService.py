50d28d16b802d4cf4ce1ad35c55eaf6ffe7fb7c3
Yasin Erduran <yasin.herduran@gmail.com>
2017-07-30 22:01:27 +0300

    added fire base demo 2

--- antService.py
+++ antService.py
@@ -1,9 +1,14 @@
 #Depencies
 #1.Flask
 # sudo pip install Flask
+#2.python firebase 1.2
+# sudo pip install requests==1.1.0
+# sudo pip install python-firebase
+
 
 from flask import Flask
 from flask_restful import Resource, Api
+from firebase import firebase
 
 app = Flask(__name__)
 api = Api(app)
